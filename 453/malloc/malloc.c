58980aab95d9f6c7bc3d23b9635023fb0a171521
Epiphane <exyphnos@gmail.com>
2015-01-12 16:25:02 -0800

    Malloc done??

--- 453/malloc/malloc.c
+++ 453/malloc/malloc.c
@@ -15,36 +15,183 @@
 
 typedef struct Block {
   size_t size;
-  struct Block *nextBlock;
+  struct Block *next;
+  struct Block *prev;
 } Block;
 
 Block *firstBlock = NULL;
+void *heapEnd = NULL;
 
 // Default page size is 64kb
 const int PAGE_SIZE = 64 * 1024;
 
-void *allocPage(int pageSize) {
-  void *success = sbrk(pageSize);
+void debugPointer(void *ptr, size_t size) {
+  printf("(ptr=%08x, size=%d)\n", ptr, size);
+}
 
-  return success;
+void *allocPage(int pageSize) {
+  void *base = sbrk(pageSize);
+
+  if(errno) {
+    fprintf(stderr, "Memory allocation failed: %s", strerror(errno));
+    return NULL;
+  }
+
+  // Keep track of where the page ends now
+  heapEnd = base + pageSize;
+
+  return base;
+}
+
+void setHeapTo(void *_heapEnd) {
+  brk(_heapEnd);
+
+  if(errno) {
+    fprintf(stderr, "Memory deallocation failed: %s", strerror(errno));
+    return;
+  }
+
+  heapEnd = _heapEnd;
+}
+
+/* Memory functions */
+Block *alloc(size_t size) {
+  Block *cursor = firstBlock;
+  Block *newBlock;
+  void *oldHeapEnd = heapEnd; // Keep track of the "before" size
+  if(!firstBlock) {
+    // Alloc our first page of memory
+    cursor = firstBlock = allocPage(PAGE_SIZE);
+    if(firstBlock == NULL) {
+      // If there was an error, return null and set errno
+      errno = ENOMEM;
+      return NULL;
+    }
+
+    // Initialize first Block
+    firstBlock->size = 0;
+    firstBlock->next = NULL;
+    firstBlock->prev = NULL;
+  }
+
+  // Now, look through blocks for a space our size
+  int foundSpot = 0;
+  while(cursor->next) {
+    // Attempt to place block after this one
+    newBlock = (void *) cursor + sizeof(Block) + cursor->size;
+    if(newBlock + size < cursor->next) {
+      newBlock->size = size;
+
+      newBlock->prev = cursor;
+      newBlock->next = cursor->next;
+      
+      cursor->next->prev = newBlock;
+      cursor->next = newBlock;
+      foundSpot = 1; // Mark that we're done!
+    }
+    // Otherwise, continue searching...
+    cursor = cursor->next;
+  }
+
+  if(!foundSpot) {
+    // If we get here, no suitable space has been found. Stick it after the end
+    cursor->next = newBlock = (void *) cursor + sizeof(Block) + cursor->size;
+    newBlock->size = size;
+    newBlock->prev = cursor;
+    newBlock->next = NULL;
+
+    while((void *) newBlock + newBlock->size > heapEnd) {
+      void *success = allocPage(PAGE_SIZE);
+      if(success == NULL) {
+        // If we had an issue, free all the pages we allocated for this
+        setHeapTo(oldHeapEnd);
+
+        // If there was an error, return null and set errno
+        errno = ENOMEM;
+        return NULL;
+      }
+    }
+  }
+
+  return newBlock;
 }
 
 void *calloc(size_t nmemb, size_t size) {
-  return 0;
+  Block *block = alloc(nmemb * size);
+  void *memory = (void *) block + sizeof(Block);
+
+  memset(memory, 0, nmemb * size);
+
+  if(getenv("DEBUG_MALLOC")) {
+    printf("MALLOC: calloc(%ld, %ld)\t=> ", nmemb, size);
+    debugPointer(memory, block->size);
+  }
+  return memory;
 }
 
 void *malloc(size_t size) {
-  if(!firstBlock) {
-    
+  Block *block = alloc(size);
+  void *memory = (void *) block + sizeof(Block);
+
+  if(getenv("DEBUG_MALLOC")) {
+    printf("MALLOC: malloc(%ld)\t=> ", size);
+    debugPointer(memory, block->size);
   }
-
-  return 0;
-}
-
-void free(void *ptr) {
-
+  return memory;
 }
 
 void *realloc(void *ptr, size_t size) {
-  return 0;
+  // Pfft, you can't free before you even allocate anything!
+  if(!firstBlock) {
+    errno = EFAULT;
+    return ptr;
+  }
+
+  // Set up a pointer to our data header
+  Block *block = ptr - sizeof(Block);
+  size_t oldSize = block->size;
+
+  void *memory = ptr;
+  // If we have space, just change the size
+  if(size <= oldSize || ptr + oldSize < block->next) {
+    block->size = size;
+  }
+  // Otherwise, we'll need a larger spot
+  else {
+    // Free my block
+    free(block);
+    // Get new spot and copy data to it
+    block = alloc(size);
+    memory = (void *) block + sizeof(Block);
+    memcpy(memory, ptr, oldSize);
+
+    // Fix pointer
+    ptr = memory;
+  }
+
+  if(getenv("DEBUG_MALLOC")) {
+    printf("MALLOC: realloc(%08x, %ld)\t=> ", ptr, size);
+    debugPointer(memory, block->size);
+  }
+
+  return ptr;
+}
+
+void free(void *ptr) {
+  // Pfft, you can't free before you even allocate anything!
+  if(!firstBlock) {
+    errno = EFAULT;
+    return;
+  }
+
+  // Set up a pointer to our data header
+  Block *block = ptr - sizeof(Block);
+
+  // Erase me from everybody's memory
+  block->prev->next = block->next;
+  if(block->next)
+    block->next->prev = block->prev;
+
+  if(getenv("DEBUG_MALLOC"))
+    printf("MALLOC: free(%08x)\n", ptr);
 }
--- 471/Program1/Program1/main.cpp
+++ 471/Program1/Program1/main.cpp
@@ -4,6 +4,31 @@
 #include "tiny_obj_loader.h"
 #include "Image.h"
 
+class Point {
+public:
+    Point() {};
+    Point(float _x, float _y, float _z) : xyz{_x, _y, _z} {};
+    
+    void transformToScreen(int width, int height) {
+        xyz[0] = (int) (xyz[0] + 1) * (width - 1) / 2;
+        xyz[1] = (int) (xyz[1] + 1) * (height - 1) / 2;
+    }
+
+    float xyz[3];
+    float getX() { return xyz[0]; }
+    float getY() { return xyz[1]; }
+    float getZ() { return xyz[2]; }
+};
+
+std::ostream &operator<< (std::ostream &out, Point point) {
+    out << "{" << point.xyz[0] << ", " << point.xyz[1] << ", " << point.xyz[2] << "}";
+    return out;
+}
+
+void decodeWithState(const char* filename);
+void drawTriangle(tinyobj::mesh_t mesh, size_t face, Image *img);
+int triangleArea(Point p1, Point p2, Point p3);
+
 int main( int argc, const char* argv[] )
 {
 	std::string inputfile = "cube.obj";
@@ -42,13 +67,79 @@ int main( int argc, const char* argv[] )
 	clr.b = 0.9;
  	// make a 640x480 image (allocates buffer on the heap)
 	Image img(400, 400);
-	// set a square to be the color above
-	for (int i=50; i < 100; i++) {
-		for (int j=50; j < 100; j++) {
-			img.pixel(i, j, clr);
-		}
-	}
+    
+    for (size_t i = 0; i < shapes.size(); i++) {
+        for (size_t f = 0; f < shapes[i].mesh.indices.size() / 3; f++) {
+            drawTriangle(shapes[i].mesh, f, &img);
+        }
+    }
+    
 	// write the targa file to disk
 	// true to scale to max color, false to clamp to 1.0
 	img.WriteTga((char *)"awesome.tga", true); 
+}
+
+void drawTriangle(tinyobj::mesh_t mesh, size_t face, Image *img) {
+    std::cout << std::endl;
+    std::vector<Point> points(3);
+    for(int i = 0; i < 3; i ++) {
+        for(int j = 0; j < 3; j ++) {
+            points[i].xyz[j] = mesh.positions[3 * mesh.indices[3 * face + i] + j];
+        }
+        points[i].transformToScreen(img->width(), img->height());
+        std::cout << points[i] << std::endl;
+    }
+    
+    float area = (float) triangleArea(points[0], points[1], points[2]);
+    
+    int min_x = points[0].getX(), max_x = points[0].getX();
+    int min_y = points[0].getY(), max_y = points[0].getY();
+    for(int i = 1; i < 3; i ++) {
+        if(points[i].getX() < min_x) min_x = points[i].getX();
+        if(points[i].getX() > max_x) max_x = points[i].getX();
+        if(points[i].getY() < min_y) min_y = points[i].getY();
+        if(points[i].getY() > max_y) max_y = points[i].getY();
+    }
+    
+    // make a color
+    color_t vertexColors[3], clr;
+    vertexColors[0].r = 1;
+    vertexColors[0].g = 0;
+    vertexColors[0].b = 0;
+    vertexColors[1].r = 0;
+    vertexColors[1].g = 1;
+    vertexColors[1].b = 0;
+    vertexColors[2].r = 0;
+    vertexColors[2].g = 0;
+    vertexColors[2].b = 1;
+    
+    Point point;
+    for (int i = min_x; i <= max_x; i ++) {
+        point.xyz[0] = i;
+        for (int j = min_y; j <= max_y; j ++) {
+            point.xyz[1] = j;
+            
+            float vertexProportions[3];
+            vertexProportions[0] = triangleArea(point, points[1], points[2]);
+            if(vertexProportions[0] < 0) continue;
+            vertexProportions[1] = triangleArea(point, points[2], points[0]);
+            if(vertexProportions[1] < 0) continue;
+            vertexProportions[2] = triangleArea(point, points[0], points[1]);
+            if(vertexProportions[2] < 0) continue;
+            
+            clr.r = clr.g = clr.b = 0;
+            for(int p = 0; p < 3; p ++) {
+                clr.r += vertexProportions[p] * vertexColors[p].r / area;
+                clr.g += vertexProportions[p] * vertexColors[p].g / area;
+                clr.b += vertexProportions[p] * vertexColors[p].b / area;
+            }
+            
+            img->pixel(i, j, clr);
+        }
+    }
+}
+
+int triangleArea(Point p1, Point p2, Point p3) {
+    return (p2.getX() - p1.getX()) * (p3.getY() - p1.getY())
+         - (p3.getX() - p1.getX()) * (p2.getY() - p1.getY());
 }
