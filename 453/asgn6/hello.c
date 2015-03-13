dce3c4502a2dc09335def27201f85a732589e615
Epiphane <exyphnos@gmail.com>
2015-03-13 13:23:23 -0700

    hello?

---
+++ 453/asgn6/hello.c
@@ -0,0 +1,15 @@
+/*
+ * Hello.c
+ * Greet the world
+ * CPE 453 - Asgn 6
+ *
+ * Thomas Steinke
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello, world!\n");
+
+  return 0;
+}
